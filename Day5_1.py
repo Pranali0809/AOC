56323e1d03262fe6e5f750a63567b969e242726b
Pranali0809 <pranali0809shinde@gmail.com>
2024-12-08 23:00:42 +0530

    5.1

--- Day5_1.py
+++ Day5_1.py
@@ -1,28 +1,30 @@
 import re
 INPUT_FILE_PATH = './input5.txt'
-myrules={'a':['b']}
-def rec(first,vec):
-    for i in vec:
-        myrules[first].append(rec(i,myrules[i]))
-    return myrules[first]
+myrules={}
         
 def main():
     with open(INPUT_FILE_PATH, 'r') as f:
         rules,updates = f.read().split("\n\n")
     rules = rules.split("\n")
     updates=updates.split("\n")   
-    
     for rule in rules:
         first,sec=rule.split("|")
         if first not in myrules.keys():
-            myrules[first]=[sec]  
-        curr=myrules[first]
-        if sec not in curr:
-            curr.append(sec)
-            myrules[first]=curr
-        print(first,":",myrules[first])
-    for i in myrules:
-        rec(i[0],i[1])
+            myrules[first] = {sec}
+        else:
+            myrules[first].add(sec)
+    
+    sum=0
+    for update in updates:
+        update=update.split(",")
+        flag=True
+        for i in range(1,len(update)):
+            if update[i-1] in myrules.keys() and update[i] in myrules[update[i-1]]:
+                continue
+            flag=False
+        if flag:
+            sum+=int(update[int(len(update)/2)])
+    print(sum)
         
 if __name__ == "__main__":
     main()
