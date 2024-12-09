052128cb3cb5f4adc9f6225224b873e928f3aee5
parth <parh1504@gmail.com>
2024-12-09 18:22:12 +0530

    6.1

---
+++ Day6_1.py
@@ -0,0 +1,47 @@
+INPUT_FILE_PATH = './input6.txt'
+
+dict={'r':[0,1],'d':[1,0],'l':[0,-1],'u':[-1,0]}
+
+def change(dir):
+    if dir=='r':
+        return 'd'
+    elif dir=='d':
+        return 'l'
+    elif dir=='l':
+        return 'u'
+    else:
+        return 'r'
+
+
+def main():
+    with open(INPUT_FILE_PATH, 'r') as f:
+        matrix = f.read().split("\n")
+    m=len(matrix)
+    n=len(matrix[0])
+    dp= [[0 for _ in range(m)] for _ in range(n)]
+    pos=[]
+    for i in range(m):
+        for j in range(n):
+            if matrix[i][j]=='^':
+                pos=[i,j]
+    print(pos)
+    dir='u'
+    count=1
+    i,j=pos
+    dp[i][j]='*'
+    while(0<=i+dict[dir][0]<m and 0<=j+dict[dir][1]<n ):
+        while( 0<=i+dict[dir][0]<m and 0<=j+dict[dir][1]<n and matrix[i+dict[dir][0]][j+dict[dir][1]]!='#'):
+            i+=dict[dir][0]
+            j+=dict[dir][1]
+            if dp[i][j]!='*':
+                dp[i][j]='*'
+                count=count+1
+                
+        if(0<=i+dict[dir][0]<m and 0<=j+dict[dir][1]<n):
+            dir=change(dir)
+    
+    print(count)
+        
+        
+if __name__ == "__main__":
+    main()
