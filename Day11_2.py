45aac62294413d0e537beeb994f183562e85b408
parth shah <parth@parths-MacBook-Air.local>
2024-12-20 21:06:04 +0530

    day 11

--- Day11_2.py
+++ Day11_2.py
@@ -1,25 +1,39 @@
-INPUT_FILE_PATH = './input11.txt'
+INPUT_FILE_PATH = './temp.txt'
+
+def rec(line,ct):
+    if ct==75:
+        return len(line)
+    j=0
+    while j<len(line):
+        word=line[j]
+        if word=='0':
+            line[j]='1'
+            j=j+1
+        elif len(word)%2==0:
+            first, second = word[:len(word)//2], word[len(word)//2:] 
+            line[j]=str(int(first))
+            line.insert(j+1,str(int(second)))
+            j=j+2
+        else:
+            line[j]=str(int(word)*2024)
+            j=j+1
+    
+    return rec(line,ct+1)
+
+
 def main():
     with open(INPUT_FILE_PATH, 'r') as f:
-        line = f.read().split(" ")
-        
-    for i in range(75):
-        j=0
-        while j<len(line):
-            word=line[j]
-            if word=='0':
-                line[j]='1'
-                j=j+1
-            elif len(word)%2==0:
-                first, second = word[:len(word)//2], word[len(word)//2:] 
-                line[j]=str(int(first))
-                line.insert(j+1,str(int(second)))
-                j=j+2
-            else:
-                line[j]=str(int(word)*2024)
-                j=j+1
+        lines = f.read().split(" ")
+    
 
-    print(len(line))        
+    
+    ans=0
+    for i in range(len(lines)):
+        ans+=rec([lines[i]],0)
+
+    print(ans)
+
+        
         
 
 
